587a20c49dbdf22554634ee05cfb33d7e396c3bb
jafortino10 <71190007+jafortino10@users.noreply.github.com>
2021-04-10 13:57:13 -0500

    Added Transaction Class

---
+++ JoeFishCoin/Transaction.java
@@ -0,0 +1,15 @@
+package JoeFishCoin;
+
+public class Transaction {
+    private String sender;
+    private String reciever;
+    private double amount;
+
+    public Transaction(String s, String r, double a) {
+        sender = s;
+        reciever = r;
+        amount = a;
+    }
+
+    
+}

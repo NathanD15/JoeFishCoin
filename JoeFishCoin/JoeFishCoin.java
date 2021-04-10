35f181f45d29dec7abd570950ac20afb97764baa
NathanD15 <33075406+NathanD15@users.noreply.github.com>
2021-04-10 01:21:30 -0500

    added todo

--- JoeFishCoin/JoeFishCoin.java
+++ JoeFishCoin/JoeFishCoin.java
@@ -9,9 +9,6 @@ class JoeFishCoin {
         JoeFishCoin.addBlock( "2", 2 );
         JoeFishCoin.addBlock( "3", 3 );
 
-        //getHash
-        //getData
-
         System.out.println("Hash :  " + JoeFishCoin.getBlockAtIndex(0).getHash() );
         System.out.println("Hash :  " + JoeFishCoin.getBlockAtIndex(1).getHash() );
         System.out.println("Hash :  " + JoeFishCoin.getBlockAtIndex(2).getHash() );

caa6d4e46a5060b2c2b3209505a30d6fa4ce40f5
NathanielDiehl <33075406+NathanielDiehl@users.noreply.github.com>
2021-05-11 10:56:02 -0500

    added new folder

--- JoeFishCoin/Account.java
+++ JoeFishCoin/Account.java
@@ -3,6 +3,27 @@ import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
-class Account { 
-    
+import javax.crypto.Cipher;
+
+
+class Account {
+    String PublicName; 
+    PrivateKey privateKey;
+    PublicKey publicKey;
+
+    Account(){
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        privateKey = keyPair.getPrivate();
+        publicKey = keyPair.getPublic();
+        PublicName = "";
+    }
+
+    Account(String name){
+        Account();
+        PublicName = name;
+    }
+
+
 }
